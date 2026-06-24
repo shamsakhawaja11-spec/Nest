32afb6863efaf2339f789167b43a62971d4aa347
Shamsa Khawaja <shamsakhawaja11@gmail.com>
2026-06-24 19:09:32 +0500

    setup jwt module in auth module

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,22 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable prettier/prettier */
+import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { PrismaService } from 'src/database/prisma.service';
+
+@Module({
+  imports: [
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: {
+        expiresIn: 60*5,
+      },
+    }),
+  ],
+  controllers: [AuthController],
+  providers: [AuthService, PrismaService],
+})
+export class AuthModule {}
