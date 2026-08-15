d24c939afc1352c065aa1f4ecc09665725f8e4bd
Shamsa Khawaja <shamsakhawaja11@gmail.com>
2026-08-15 21:15:28 +0500

    update: Config Module

--- src/common/ConfigDemoService.ts
+++ src/common/ConfigDemoService.ts
@@ -1,7 +1,7 @@
 import { Inject, Injectable } from "@nestjs/common";
 import { APP_CONFIG } from "./configModule";
 
-Injectable()
+@Injectable()
 export class ConfigDemoService{
     constructor(@Inject('APP_CONFIG')private appConfig:typeof APP_CONFIG){}
     async getConfig(){
